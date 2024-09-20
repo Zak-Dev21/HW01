163d0adb4865eb01eb1b8f8cc04e3816b39087c4
Zak <zakariabouaddou@gmail.com>
2024-09-19 17:31:45 -0700

    writing initial pseudocode for binary representation

--- HW01.cpp
+++ HW01.cpp
@@ -1,7 +1,22 @@
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    
+    int user_num;
+    int quotient;
+    int remainder;
+    int prev;
+    int binary;
+
+    while (quotient != 0) {
+        prev = user_num;
+        quotient = user_num / 2;
+        remainder = user_num / 2;
+        prev = quotient;
+        //binary (concatenate with remainder)
+    }
+
+    cout << binary;
 }
