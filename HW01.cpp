1ccb95a855473507032022a0de4086273da95fe4
Zak <zakariabouaddou@gmail.com>
2024-09-19 17:38:52 -0700

    adjusted code to include user input and also initialize the quotient

--- HW01.cpp
+++ HW01.cpp
@@ -7,13 +7,17 @@ int main()
     int user_num;
     int quotient;
     int remainder;
-    int prev;
-    int binary;
+    char binary[32]; //used to hold binary string
+    int index = 0; // used to track position in binary string
+
+    cout << "Enter a positive integer number: ";
+    cin >> user_num;
+
+    quotient = user_num;
+
 
     while (quotient != 0) {
-        prev = user_num;
-        quotient = user_num / 2;
-        remainder = user_num / 2;
+        remainder = quotient % 2;
         prev = quotient;
         //binary (concatenate with remainder)
     }
